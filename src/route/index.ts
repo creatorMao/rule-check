09fb414dc0988feba517c4388863e79262fcc2e7
Zhiwu Mao <zwmao@outlook.com>
2022-09-23 16:38:39 +0800

    增加const路由

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,8 +1,10 @@
 import Home from '../view/Home.vue'
+import Const from '../components/Setting/Const.vue'
 import { createRouter, createWebHashHistory } from "vue-router"
 
 const routes = [
-  { path: '/', component: Home }
+  { path: '/', component: Home },
+  { path: '/config/Const', component: Const }
 ]
 
 const router=createRouter({
