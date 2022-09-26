d590b508f13b6e826e2aebb7909a9ffc8fe51e9a
Zhiwu Mao <zwmao@outlook.com>
2022-09-27 00:45:23 +0800

    去除多余的promise

--- src/model/config/constGroupModel.ts
+++ src/model/config/constGroupModel.ts
@@ -1,26 +1,16 @@
 import {http} from '../../helper/requestHelper'
 
 const deleteConstGroup = async (idList: Array<any>) => {
-  return new Promise((resolve,reject)=>{
-    http.request('/config/constGroup/delete', 'post', {
+  return http.request('/config/constGroup/delete', 'post', {
       idList: JSON.stringify(idList)
     })
-    .then((res: any) => {
-      resolve(res);
-    })
-  })
 }
 
 const getConstGroupList = (pageIndex: Number, pageSize: Number) => {
-  return new Promise((resolve,reject)=>{
-    http.request('/config/constGroup/get', 'post', {
+  return http.request('/config/constGroup/get', 'post', {
       pageIndex,
       pageSize
     })
-    .then((res: any) => {
-      resolve(res);
-    })
-  })
 }
 
 export {
