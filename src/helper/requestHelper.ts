7c444dac619bb2f8fedd2fec06178bd48df6c22f
Zhiwu Mao <zwmao@outlook.com>
2022-10-09 22:57:59 +0800

    axios，改用拦截器处理异常错误

--- src/helper/requestHelper.ts
+++ src/helper/requestHelper.ts
@@ -5,6 +5,31 @@ const http: any = axios.create({
   baseURL: 'http://192.168.1.201:3000/',
 });
 
+http.interceptors.request.use(function (config) {
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
+
+http.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  const { code } = error;
+  console.log(error);
+  let message = '请求失败！';
+  switch (code) {
+    case 'ERR_NETWORK':
+      message = '请求地址网络错误，请检查对应接口是否启动服务！'
+      break;
+    default:
+      break;
+  }
+  createErrorMessage(message, 2000);
+
+  
+  return Promise.reject(error);
+});
+
 http.request = (url: String, method: String, param: any) => {
   return new Promise((resolve, reject) => {
     http({
@@ -18,17 +43,6 @@ http.request = (url: String, method: String, param: any) => {
         resolve(data);
       })
       .catch((err: any) => {
-        const { code } = err;
-        console.log(err);
-        let message = '请求失败！';
-        switch (code) {
-          case 'ERR_NETWORK':
-            message = '请求地址网络错误，请检查对应接口是否启动服务！'
-            break;
-          default:
-            break;
-        }
-        createErrorMessage(message, 2000);
         reject(err)
       })
   })
